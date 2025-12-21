2aaf05e2fea76d486ff6a869ca4c30aae2924ccc
Nicolas Maitre <nico.maitre.stair@gmail.com>
2025-12-21 13:34:19 +0100

    FEATURE: add analytics

---
+++ app/components/Analytics.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Analytics() {
+	useEffect(() => {
+		// Only run on client side
+		if (typeof window !== "undefined") {
+			import("@plausible-analytics/tracker").then(({ init }) => {
+				init({
+					domain: "ondama.fr",
+					endpoint: "https://analytics.nicolasmaitre.dev",
+				});
+			});
+		}
+	}, []);
+
+	return null;
+}
